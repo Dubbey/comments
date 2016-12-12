bea38c15e3ca0c281f3cd06a99538221bc99533f
Dubbey <plami.petrov@gmail.com>
2016-12-12 23:58:53 +0100

    added basic configuration to the module

--- comments.php
+++ comments.php
@@ -3,14 +3,20 @@
 class Comments extends Module
 {
 
-  function __construct(argument)
+  function __construct()
   {
     $this->name = 'comments';
     $this->tab = 'front_office_features';
+    $this->author = 'Dubby';
     $this->version= '0.1';
     $this->displayName = 'My comments module';
     $this->description = 'This module allows your customers to grade and comment you products';
     parent::__construct();
+  }
+
+  public function getContent()
+  {
+    return 'Config test.';
   }
 }
 
