9101de5ba1b05382b8e030ffdb22c94e9bc36f1e
Dubbey <plami.petrov@gmail.com>
2016-12-13 00:41:35 +0100

    added the option to save the configuration values in the database

--- comments.php
+++ comments.php
@@ -11,14 +11,28 @@ class Comments extends Module
     $this->version= '0.1';
     $this->displayName = 'My comments module';
     $this->description = 'This module allows your customers to grade and comment you products';
+    $this->bootstrap = true;
     parent::__construct();
   }
 
   public function getContent()
   {
+    $this->processConfiguration();
     return $this->display(__FILE__,'getContent.tpl');
   }
-}
 
+  public function processConfiguration()
+  {
+    if (Tools::isSubmit('mymod_pc_form'))
+    {
+      $enable_grades = Tools::getValue('enable_grades');
+      $enable_comments = Tools::getValue('enable_comments');
+      Configuration::updateValue('MYMOD_GRADES', $enable_grades);
+      Configuration::updateValue('MYMOD_COMMENTS', $enable_comments);
+      
+    }
+  }
+
+}
 
  ?>
